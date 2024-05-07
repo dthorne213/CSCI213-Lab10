a5fb9c297eaa4c6b20541707a1c6877a27ed15de
Dayla Thorne <DaylaThorne@go.rmc.edu>
2024-05-07 11:01:47 -0400

    Added Car.java

---
+++ Car.java
@@ -0,0 +1,65 @@
+/**
+* Lab 10 Car class
+* @author Dayla Thorne 
+*
+*/
+
+
+
+public class Car extends Vehicle {
+	private int numDoors; 
+	private int numPassengers; 
+	
+	public Car(String aMake, String aModel, String aPlate, int aDoors, int aPassengers){
+		super(aMake, aModel, aPlate);
+		this.numDoors = aDoors; 
+		this.numPassengers = aPassengers; 
+	}
+		
+		public int getDoors(){
+			return this.numDoors; 
+		} 
+		
+		public int getPassengers(){
+			return this.numPassengers; 
+		}
+		
+		public String toString() {
+		String result = String.format("%d-door %s%s with license %s.", this.numDoors, this.getMake(), this.getMake(), this.getPlate());
+		return result;
+		}
+		
+		@Override 
+		public boolean equals(Object other) {
+			if(!(other instanceof Vehicle)) {
+				return false;
+			}
+			
+			Car otherCar = (Car) other;
+			
+			if (this.numDoors == otherCar.getDoors()){
+				if (this.numPassengers == otherCar.getPassengers()){
+                return super.equals(other);
+			}
+			
+			}
+		
+		
+		return false;
+		}
+		
+		
+		public Car copy(){
+			String aMake = this.getMake(); 
+			String aModel = this.getModel(); 
+			String aPlate = this.getPlate(); 
+			int numDoors = this.getDoors(); 
+			int numPassengers = this.getPassengers(); 
+			Car copy = new Car(aMake,aModel,aPlate,numDoors,numPassengers); 
+			return copy; 
+		}
+		
+}
+
+        
+		
